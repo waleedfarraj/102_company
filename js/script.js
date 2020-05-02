44169e218cbd8784b622bccfaa9f1ed8778f791d
waleedfarraj <waleed.farraj95@gmail.com>
2020-05-03 01:19:51 +0300

    lab08

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 "use strict";
+
 function getTime(){
 var today = new Date();
 var hourNow = today.getHours();
@@ -29,4 +30,27 @@ function hala(){
     welcome ="Even if you can't answer a yes or no question you are welcome here  <3 "
 }
 return welcome;
+}
+
+function vaildation(){
+  var usrinput = prompt("are you excited to visit jordan ?you can only answer yes !")
+  while ( usrinput != "yes" ){
+    prompt("are you excited to svisit jordan ?you can only answer yes !")
+    if (usrinput == 'yes')
+    break; 
+  }
+return usrinput;
+}
+var item
+function k()
+ {
+  var kay = prompt("how many plates of knafah do you want ?write it in number form")
+   item ='<img src="https://1.bp.blogspot.com/-twYs6GQdKdQ/Vh8API1rZ8I/AAAAAAAAAtU/OSQI-p1zChM/s1600/Kunafa-traditional-arabic-dessert-cuisine-everyday-arabic-recipe.jpg">';
+ 
+
+  for ( var i=0 ; i<kay ; i=i+1) {
+item = item +'<img src="https://1.bp.blogspot.com/-twYs6GQdKdQ/Vh8API1rZ8I/AAAAAAAAAtU/OSQI-p1zChM/s1600/Kunafa-traditional-arabic-dessert-cuisine-everyday-arabic-recipe.jpg">';
+  }
+  console.log (item)
+  return item;
 }
